94002a4d820fd6064cd14708a8deae0e8b97609e
takitazwar8 <takitazwar.parthib@gridty.com>
2025-05-11 04:48:03 +0200

    add: added api call

---
+++ src/app/DataFetcher.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+async function fetchData() {
+  const res = await fetch(process.env.NEXT_PUBLIC_API_URL as string);
+  const data = await res.json();
+  return data;
+}
+
+export default async function DataFetcher() {
+  const result = await fetchData();
+
+  return (
+    <div className='flex flex-row gap-4'>
+      {result?.data?.map((users:{id:string,name:string,age:string})=>{
+
+        return <div>
+            <h2>Id: {users.id}</h2>
+            <h2>Name: {users.userName}</h2>
+            <h2>Age: {users.age}</h2>
+        </div>
+      })}
+    </div>
+  );
+}
