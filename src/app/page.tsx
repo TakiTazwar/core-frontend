fd36fde00f87311ad476337a2565d2375565a1fd
takitazwar8 <takitazwar.parthib@gridty.com>
2025-05-16 01:40:11 +0200

    fix: dark mode text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen w-screen ">
       <Header />
-      <main className="flex flex-col flex-grow bg-gray-100 items-center justify-center">
+      <main className="flex flex-col flex-grow bg-gray-100 items-center justify-center text-black">
         <h1>Page is Under Construction!</h1>
         <p>Sample Database Values: </p>
         <DataFetcher />
