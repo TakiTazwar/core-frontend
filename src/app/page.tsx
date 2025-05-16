1ec2c818d76103d4abc452b8faaf0572fe3a60d0
takitazwar8 <takitazwar.parthib@gridty.com>
2025-05-16 21:40:01 +0200

    add: title color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
       <main className="flex flex-col flex-grow bg-gray-100 items-center text-black">
         <div className="flex flex-row w-full h-150 p-10 bg-[#1E1E1E] shadow-md">
           <div className="w-[60%] h-full flex flex-col justify-center p-36 text-white">
-            <h1 className="text-5xl font-bold">Innovating Beyond Boundaries,</h1>
-            <h1 className="text-5xl font-bold pb-8">Redefining Possibilities.</h1>
+            <h1 className="text-5xl font-bold bg-custom-gradient">Innovating Beyond Boundaries,</h1>
+            <h1 className="text-5xl font-bold pb-8 bg-custom-gradient">Redefining Possibilities.</h1>
             <p>Harness the power of modern web technologies and cloud-native solutions to deliver scalable, high-performance WordPress products. Combining frameworks like Next.js and Nuxt.js with robust backend tools such as Django and FastAPI, supported by leading cloud platforms and container orchestration, we create seamless, secure, and future-ready experiences.</p>
           </div>
           <div className="w-[40%] h-full">
